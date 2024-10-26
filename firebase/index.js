77d853b254e70d592afbc541996125b5f2744c87
quangtai1804 <tailqph26595@fpt.edu.vn>
2024-10-26 22:48:21 +0700

    Create Form Product, Add product, show product

--- firebase/index.js
+++ firebase/index.js
@@ -2,10 +2,10 @@ const admin = require("firebase-admin");
 
 // Initialize firebase admin SDK
 
-const serviceAccount = require("../datn-de212-15d26-firebase-adminsdk-y38me-464dec9913.json");
+const serviceAccount = require("../beefoodconsole-69415b4d38ee.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "datn-de212-15d26.appspot.com",
+  storageBucket: "beefoodconsole.appspot.com",
 });
 // Cloud storage
 const bucket = admin.storage().bucket();
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 const session = require("express-session");
 const { yeu_cau_dang_nhap } = require("../middleware/checklogin");
+var product = require("../controllers/product.controller");
 
 var router = express.Router();
 
@@ -9,5 +10,33 @@ router.get("/", yeu_cau_dang_nhap);
 router.get("/home", function (req, res, next) {
   res.render("home", { title: "Express" });
 });
+
+router.get("/home", function (req, res, next) {
+  res.render("home", { title: "Express" });
+});
+
+//Product
+router.get("/addProduct", function (req, res, next) {
+  res.render("product/addProduct", { title: "Express", req: req });
+});
+
+router.get("/showProduct", async function (req, res, next) {
+  const data = await product.dataProductRestaurant(req, res);
+  res.render("product/showProduct", {
+    list: data,
+    req: req,
+  });
+});
+
+router.get("/editProduct/:id", async function (req, res, next) {
+  console.log(req.params.id);
+  const data = await productModel.productModel.findById({ _id: req.params.id });
+  res.render("product/editProduct", {
+    product: data,
+    req: req,
+  });
+});
+
+router.get("/listproduct", product.getListProduct);
 
 module.exports = router;
