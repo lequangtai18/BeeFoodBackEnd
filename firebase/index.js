5523a6055ae9c48dc14122a59603deaefbc7bd03
Truong Tu <tutvph14566@fpt.edu.vn>
2025-01-14 23:42:28 +0700

    bo duyet sp admin

--- firebase/index.js
+++ firebase/index.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 
 // Initialize firebase admin SDK
 
-const serviceAccount = require("../beefoodconsole-firebase-adminsdk-xv6qz-b1d09632eb.json");
+const serviceAccount = require("../beefoodconsole-firebase-adminsdk-xv6qz-10720fc2f9.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: "beefoodconsole.appspot.com",
