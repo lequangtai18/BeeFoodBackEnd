d6edebda6bfd36784a8b71fd2ddea54e4abee7d7
quangtai1804 <tailqph26595@fpt.edu.vn>
2025-01-16 07:59:21 +0700

    Update Detail order

--- firebase/index.js
+++ firebase/index.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 
 // Initialize firebase admin SDK
 
-const serviceAccount = require("../beefoodconsole-firebase-adminsdk-xv6qz-5840793a6d.json");
+const serviceAccount = require("../beefoodconsole-firebase-adminsdk-xv6qz-4281ee470d.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: "beefoodconsole.appspot.com",
